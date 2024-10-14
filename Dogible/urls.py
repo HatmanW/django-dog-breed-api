0391c76f9ae4217e02efd9885319b4b7783210f1
HatmanW <jwinchester@unomaha.edu>
2024-10-14 03:12:09 -0500

    Version 5. Viewsets.

--- Dogible/urls.py
+++ Dogible/urls.py
@@ -1,30 +1,8 @@
 #Dogible\urls.py
-"""
-URL configuration for Dogible project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
-'''this part is always one of those engmatic parts of django
-I remember John Elder explaining the concept 
-of going to the main app, and pointing one URL.py file to another
-then quote "never having to touch the main application URL.py again." 
-or something similar.'''
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('dogapi.urls'))
-
+    path('admin/', admin.site.urls),  # Django admin URL
+    path('api/', include('dogapi.urls')),  # Include the dogapi URLs
 ]
--- dogapi/urls.py
+++ dogapi/urls.py
@@ -1,12 +1,9 @@
 #dogapi\urls.py
-from django.urls import path
-from .views import DogList, DogDetail, BreedList, BreedDetail
+from rest_framework.routers import DefaultRouter
+from .views import DogViewSet, BreedViewSet
 
-'''this was probably the more confusing part. It's a URL that points to URLS similar to how we did in ISQA3900
-you can point the main application URL to look at the other url.py and use those, or just have ONE huge URL.py'''
-urlpatterns = [
-    path('api/dogs/', DogList.as_view(), name='dog-list'),
-    path('api/dogs/<int:pk>/', DogDetail.as_view(), name='dog-detail'),
-    path('api/breeds/', BreedList.as_view(), name='breed-list'),
-    path('api/breeds/<int:pk>/', BreedDetail.as_view(), name='breed-detail'),
-]
+router = DefaultRouter()
+router.register(r'dogs', DogViewSet)
+router.register(r'breeds', BreedViewSet)
+
+urlpatterns = router.urls
